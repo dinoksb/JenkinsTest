52219d32a43745a01759ee1daa4fb93830cd6558
SeungBaeKim <sb.kim@morph.kr>
2019-09-25 12:00:16 +0900

    Test Commit

--- Assets/MyTestScript.cs
+++ Assets/MyTestScript.cs
@@ -7,7 +7,7 @@ public class MyTestScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Jenkins Test");
     }
 
     // Update is called once per frame
